6f19a9463fbd75bcb796ae4342c17dd3bcdfb810
Peter Maitland <pmaitland98@gmail.com>
2018-11-08 15:11:07 +0000

    day on calendar now coloured by most frequent mood that day

--- Mood-for-Thought/screens/CalendarScreen.js
+++ Mood-for-Thought/screens/CalendarScreen.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { ScrollView, StyleSheet, View, Text } from 'react-native';
+import {
+    getDay,
+    getAllDays,
+} from '../services/day-service';
 import baseStyles from '../styles/base';
 import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import { MoodColors } from '../constants/Colors';
 
 
 export default class CalendarScreen extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-           date: 'date'
+           day: null,
+           allDays: []
         }
      }
 
@@ -16,16 +22,77 @@ export default class CalendarScreen extends React.Component {
         title: 'Calendar',
     };
 
-    componentDidMount = () => {
+    componentDidMount() {
         this.updateDetails(new Date());
+        this.getAllDays();
     }
 
-    updateDetails = (date) => {
-        this.setState({date: date});
-        console.log(date);
+    async getAllDays() {
+        this.setState({allDays: await getAllDays()});
+    }
+
+    async updateDetails(date) {
+        this.setState({day: await getDay(date)});
+    }
+
+    mostCommonElement(array) {
+      var occurences = {};
+      var mc      = array[0],
+          mcCount = 1;
+      for (var i = 0; i < array.length; i++) {
+        var el = array[i];
+        if(occurences[el] == null)
+            occurences[el] = 1;
+        else
+            occurences[el]++;
+        if(occurences[el] > mcCount) {
+          mc = el;
+          mcCount = occurences[el];
+        }
+      }
+      return mc;
     }
 
     render() {
+        const day  = this.state.day;
+        const allDays = this.state.allDays;
+        var date, logs, moods;
+
+        { day ?
+          date = day.date :
+          date = null
+        }
+
+        { day ?
+          logs = day.logs :
+          logs = null
+        }
+
+        { logs ?
+          moods = logs.map(log => log.mood) :
+          moods = null
+        }
+
+        let markedDates = {};
+        allDays.forEach((day) => {
+          markedDates = {
+            ...markedDates,
+            [day.date.substring(0,10)]: {
+              customStyles: {
+                container: {
+                  backgroundColor: MoodColors[this.mostCommonElement(day.logs.map((log) => log.mood))],
+                  borderRadius: 10,
+                  width: 48,
+                },
+                text: {
+                  color: 'black',
+                  fontWeight: 'bold'
+                },
+              },
+            }
+          };
+        });
+
         return (
             <ScrollView style={baseStyles.container}>
               <CalendarList
@@ -39,43 +106,19 @@ export default class CalendarScreen extends React.Component {
                 onPressArrowRight={addMonth => addMonth()}
 
                 markingType={'custom'}
-                markedDates={{
-                  '2018-11-01': {
-                    customStyles: {
-                      container: {
-                        backgroundColor: 'green',
-                        borderRadius: 0,
-                        width: 64,
-                      },
-                      text: {
-                      },
-                    },
-                  },
-                  '2018-11-08': {
-                    customStyles: {
-                      container: {
-                        backgroundColor: 'green',
-                        borderRadius: 0,
-                        width: 64,
-                      },
-                      text: {
-                      },
-                    },
-                  },
-                  '2018-11-09': {
-                    customStyles: {
-                      container: {
-                        backgroundColor: 'green',
-                        borderRadius: 0,
-                        width: 64,
-                      },
-                      text: {
-                      },
-                    }
-                  }}}
+                markedDates={markedDates}
               />
               <View style={styles.getStartedContainer}>
-                  <Text style={styles.getStartedText}>{this.state.date.toString()}</Text>
+                    { day ?
+                      <Text style={styles.getStartedText}>{day.date}</Text> :
+                      <Text style={styles.getStartedText}>No data to display for this day.</Text>
+                    }
+                    { day ?
+                      <Text style={styles.getStartedText}>
+                        {moods.map(mood => mood.toString())}
+                      </Text> :
+                      <Text style={styles.getStartedText}>No data to display for this day.</Text>
+                    }
               </View>
             </ScrollView>
         );
