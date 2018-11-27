cd5b0edf99f60a7561e724ef8147bed3d044238d
Peter Maitland <pmaitland98@gmail.com>
2018-11-27 21:00:33 +0000

    mood timeline

--- Mood-for-Thought/screens/CalendarScreen.js
+++ Mood-for-Thought/screens/CalendarScreen.js
@@ -6,8 +6,11 @@ import {
 } from '../services/day-service';
 import baseStyles from '../styles/base';
 import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
-import { MoodColors } from '../constants/Colors';
+import Colors from '../constants/Colors';
 import { Svg } from 'expo';
+import format from 'dateformat';
+
+const MoodColors = Colors.MoodColors;
 
 export default class CalendarScreen extends React.Component {
     constructor(props) {
@@ -53,6 +56,30 @@ export default class CalendarScreen extends React.Component {
       return mc;
     }
 
+    getTimelineMarkerWidth() {
+      console.log("width:", (Dimensions.get('window').width - 50) / (24 * 60));
+      return (Dimensions.get('window').width - 50) / (24 * 60);
+    }
+
+    getTimelineMarkerPosition(time) {
+      var hours = parseInt(time.split(':')[0]);
+      var mins = parseInt(time.split(':')[1]);
+
+      var markerWidth = this.getTimelineMarkerWidth();
+
+      console.log(time, hours, mins);
+      console.log((hours * 60 + mins) * markerWidth);
+      return Math.floor(15 + (hours * 60 + mins) * markerWidth);
+    }
+
+    getDaySteps() {
+      return this.state.day ? this.state.day.steps : 0;
+    }
+
+    getDaySleep() {
+      return this.state.day ? this.state.day.steps : 0;
+    }
+
     render() {
         const day  = this.state.day;
         const allDays = this.state.allDays;
@@ -76,9 +103,10 @@ export default class CalendarScreen extends React.Component {
         let markedDates = {};
         allDays.forEach((day) => {
           let mostCommonMood = this.mostCommonElement(day.logs.map((log) => log.mood));
+          let dateString = format(day.date, 'yyyy-mm-dd');
           markedDates = {
             ...markedDates,
-            [day.date.substring(0,10)]: {
+            [dateString]: {
               customStyles: {
                 container: {
                   backgroundColor: MoodColors[mostCommonMood],
@@ -93,8 +121,6 @@ export default class CalendarScreen extends React.Component {
             }
           };
         });
-
-        const moodTimelineWidth = Dimensions.get('window').width;
 
         var mostCommonMood;
         var moodTimelineColor;
@@ -113,23 +139,27 @@ export default class CalendarScreen extends React.Component {
 
         // console.log(moodTimelineColor);
 
-        let markedTimes = {};
+        var markedTimes = {};
 
         if (day) {
           day.logs.forEach((log) => {
+            let timeString = format(log.time, 'HH:MM');
             markedTimes = {
               ...markedTimes,
-              [log.time.substring(11,16)]: {
+              [timeString]: {
                 color: MoodColors[log.mood]
               }
-            };
+            }
           });
-          console.log(day.logs);
         }
 
         console.log(markedTimes);
 
+        const moodTimelineWidth = Dimensions.get('window').width - 35;
+        const moodTimelineHeight = 50;
+
         return (
+          <ScrollView>
             <ScrollView style={baseStyles.container}>
               <CalendarList
                 horizontal={true}
@@ -143,16 +173,76 @@ export default class CalendarScreen extends React.Component {
                 markingType={'custom'}
                 markedDates={markedDates}
               />
-              <Svg height={100} width={moodTimelineWidth}>
-                <Svg.Rect
-                  x={15}
-                  y={15}
-                  width={moodTimelineWidth - 30}
-                  height={50}
-                  fill={moodTimelineColor}
-                />
-              </Svg>
             </ScrollView>
+
+            <View style={{padding: 10}}>
+              <Text>Log Timeline</Text>
+
+              <Svg height={75} width={moodTimelineWidth}>
+
+              // timeline background
+              <Svg.Rect
+                x={15}
+                y={15}
+                width={moodTimelineWidth}
+                height={moodTimelineHeight}
+                fill={"#fff"}
+              />
+
+              // scale markings on timeline
+              <Svg.Rect
+                key={"scaleMarker6"}
+                x={this.getTimelineMarkerPosition("06:00")}
+                y={15}
+                width={1}
+                height={moodTimelineHeight}
+                fill={'lightgrey'}
+              />
+              <Svg.Rect
+                key={"scaleMarker12"}
+                x={this.getTimelineMarkerPosition("12:00")}
+                y={15}
+                width={1}
+                height={moodTimelineHeight}
+                fill={'lightgrey'}
+              />
+              <Svg.Rect
+                key={"scaleMarker18"}
+                x={this.getTimelineMarkerPosition("18:00")}
+                y={15}
+                width={1}
+                height={moodTimelineHeight}
+                fill={'lightgrey'}
+              />
+
+              // log markings on timeline
+              {
+                Object.entries(markedTimes).reverse().map(([key, value]) => (
+                  <Svg.Rect
+                    key={key}
+                    x={this.getTimelineMarkerPosition(key)}
+                    y={10}
+                    width={5}
+                    height={moodTimelineHeight + 10}
+                    fill={value.color}
+                  />
+                ))
+              }
+              </Svg>
+
+              <View style={{flex: 1,
+                            flexDirection: 'row',
+                            justifyContent: 'space-between'}}>
+                <Text style={{color: 'grey'}}>00:00</Text>
+                <Text style={{color: 'grey'}}>06:00</Text>
+                <Text style={{color: 'grey'}}>12:00</Text>
+                <Text style={{color: 'grey'}}>18:00</Text>
+                <Text style={{color: 'grey'}}>00:00</Text>
+              </View>
+
+              <Text>You walked {this.getDaySteps()} steps and slept for {this.getDaySleep()} hours on this day.</Text>
+            </View>
+          </ScrollView>
         );
     }
 }
